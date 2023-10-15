5ae162ac6304eaa0de45295be80507a42068fc69
sikorskayaX <sikorskaya02@gmail.com>
2023-10-15 23:25:45 +0300

    Enhance dataset generation and classification in neir.py
    This commit significantly improves the dataset generation and classification process in the neir.py file. The number of data points has been increased from 10 to 20, and the data points are now divided into four classes instead of two.
    
    The commit also introduces the use of the copy module to create deep copies of the dataset. This allows for the creation of two new datasets, dataset1 and dataset2, which combine different classes of data points.
    
    The prediction process has been updated to handle the four classes of data points, and the graph plotting process has been updated to color-code the four classes of data points differently.
    
    Finally, the commit adds two lines to the graph to separate the four classes of data points.

--- neir.py
+++ neir.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import copy
 
 #function of predict
 
@@ -26,36 +27,93 @@ def train_we(train,lr,n_epoch):
                 weights[i]+=lr*error*row[i]
     return weights, delta #list of weights, deltas
 
-#random points
-N = 10
+#create dataset to save points
+N = 20 
 dataset = [[0]*4]*N
+dataset1 = [[0]*4]*N
+dataset2 = [[0]*4]*N
 
-x1 = np.random.random(N)
-x2 = x1 + [np.random.randint(10)/10 for i in range(N)]
-x3 = np.random.random(N)
-x4 = x3 - [np.random.randint(10)/10 for i in range(N)] - 0.1
 
-for i in range(10):
+# points for class 0
+x1_0 = np.random.uniform(0.5 , 1.5, N)
+x2_0 = np.random.uniform(0.5 , 1.5, N)
+
+# points for class 1
+x1_1 = np.random.uniform(-0.5 , -1.5, N)
+x2_1 = np.random.uniform(-0.5 , -1.5, N)
+
+# points for class 2
+x1_2 = np.random.uniform(-0.5 , -1.5, N)
+x2_2 = np.random.uniform(0.5 , 1.5, N)
+
+# points for class 3
+x1_3 = np.random.uniform(0.5 , 1.5, N)
+x2_3 = np.random.uniform(-0.5 , -1.5, N)
+
+# dividing points into 4 classes
+for i in range(N):
     if i < 5:
-        dataset[i] = [1, x1[i], x2[i], 0]
+        dataset[i] = [1, x1_0[i], x2_0[i], 0]
+    elif (i >= 5) and ( i < 10):
+        dataset[i] = [1, x1_1[i], x2_1[i], 1]
+    elif (i >= 10) and ( i < 15):
+        dataset[i] = [1, x1_2[i], x2_2[i], 2]
     else:
-        dataset[i] = [1, x3[i], x4[i], 1]
+        dataset[i] = [1, x1_3[i], x2_3[i], 3] 
+        
         
 l_rate=0.1 #step of bias
 n_epoch=1000 #number of epochs to train
 weights, delta =train_we(dataset,l_rate,n_epoch) #final weight and bias
 
+#copy
+dataset1  = copy.deepcopy(dataset)
+dataset2  = copy.deepcopy(dataset)
+
+ 
+#combining 0 and 4 classes
+for i in range (N):
+    if (i < 5) or (i >= 15):
+        dataset1[i][3] = 0
+    else:
+        dataset1[i][3] = 1
+    
+#combining 2 and 3 classes
+for i in range (N):
+    if (i < 5) or(10 <= i <15):
+        dataset2[i][3] = 0
+    else:
+        dataset2[i][3] = 1
+
+
+weights_1, delta_1 =train_we(dataset1,l_rate,n_epoch) 
+weights_2, delta_2 =train_we(dataset2,l_rate,n_epoch)
+
 #compare the prediction with what we have chosen
 for row in dataset:
-    predictit= predict(row,weights,delta)
-    print('selected:%d , predicted:%d ' % (row[-1],predictit))
-
-#dividing points into 2 groups and creating a graph
-for row in dataset:
-    if row[-1] == 1:
-        plt.scatter(row[1],row[2],s=10, c='red')
+    if row[-1] == 0:
+        predictit= predict(row,weights_1,delta_1)
+        print('selected:%d , predicted:%d ' % (row[-1],predictit)) 
+    elif row[-1] == 1:
+        predictit= predict(row,weights_1,delta_1)
+        print('selected:%d , predicted:%d ' % (row[-1],predictit)) 
+    elif row[-1] == 2:
+        predictit= predict(row,weights_2,delta_2)+ 2
+        print('selected:%d , predicted:%d ' % (row[-1],predictit)) 
     else:
-        plt.scatter(row[1],row[2],s=10, c='blue')
+        predictit= predict(row,weights_2,delta_2)+ 2
+        print('selected:%d , predicted:%d ' % (row[-1],predictit))
+
+#colors of points on graph
+for row in dataset:
+    if row[-1] == 0:
+        plt.scatter(row[1],row[2],s=10, c='red')
+    elif row[-1] == 1:
+        plt.scatter(row[1],row[2],s=10, c='orange')
+    elif row[-1] == 2:
+        plt.scatter(row[1],row[2],s=10, c='green')
+    else:
+        plt.scatter(row[1],row[2],s=10, c='blue') 
 
 #line separating the classes
 def line(x1,weights):
@@ -63,7 +121,8 @@ def line(x1,weights):
 
 #plots a line using the given weights on a graph
 x1_range = np.arange(-5,5,0.5)
-plt.plot(x1_range, line(x1_range, weights), color='black')
+plt.plot(x1_range, line(x1_range, weights_1), color='black') 
+plt.plot(x1_range, line(x1_range, weights_2), color='black') 
 plt.grid(True)
 plt.show()
 
